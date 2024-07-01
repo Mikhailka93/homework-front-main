c25eb6b53491e5369facf8496334240f49cfc8ae
Michael Kazansky <mikhail.ka93@gmail.com>
2024-07-01 15:51:45 +0400

    HW1

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,33 @@
+type AddressType = {
+    street: string
+    city: string
+};
+
+type UserType = {
+    id: number
+    name: string
+    age: number
+    address: AddressType
+};
+
+type UserListPropsType = {
+    users: UserType[]
+};
+
+export const UserList = (props: UserListPropsType) => {
+    const {users} = props
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
+
+            <ul>
+                {users.map((el: UserType) => (
+                    <li key={el.id} id={`hw01-user-${el.id}`}>
+                        <strong>{el.name}</strong> (Age: {el.age})<strong> Address:</strong>
+                        {el.address.street}, {el.address.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
